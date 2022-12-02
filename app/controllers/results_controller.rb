e774d7c3855ecdce2a9437e22e7d019d8c178397
jonathanmpope <jonathanmpope@gmail.com>
2022-12-01 17:26:04 -0700

    feat: add conditioning info to results page with feature test

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,6 +7,10 @@ class ResultsController < ApplicationController
         @deadlift = @profile.exercises.where(name: "Straight Bar").or(@profile.exercises.where(name: "Trap Bar"))[0] 
         @press = @profile.exercises.where(name: "Bench Press").or(@profile.exercises.where(name: "Overhead Press (1 Arm)"))[0] 
         @weighted_pullup = @profile.exercises.where(name: "Weighted Pull-up")[0] 
+        @extended_capacity = @profile.exercises.where(name: "8 mile ruck")[0] 
+        @capacity = @profile.exercises.where(name: "5 mile run")[0] 
+        @extended_power = @profile.exercises.where(name: "1.5 mile run")[0] 
+        @power = @profile.exercises.where(name: "400m run")[0] 
     end 
 
     private
--- spec/features/results/show_spec.rb
+++ spec/features/results/show_spec.rb
@@ -15,17 +15,24 @@ RSpec.describe 'the landing page' do
         click_on "Login"
 
         @profile = user.profiles.create!(weight: 195.0, track: "sof_land_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
+        
         @squat = @profile.exercises.create!(category: 'strength', name: "Back Squat", value: 355)
         @deadlift = @profile.exercises.create!(category: 'strength', name: "Trap Bar", value: 455)
         @press = @profile.exercises.create!(category: 'strength', name: "Bench Press", value: 275)
         @weighted_pullup = @profile.exercises.create!(category: 'strength', name: "Weighted Pull-up", value: 260)
+
+        @extended_capacity = @profile.exercises.create!(category: 'conditioning', name:'8 mile ruck', value: 120)
+        @capacity = @profile.exercises.create!(category: 'conditioning', name:'5 mile run', value: 35.5)
+        @extended_power = @profile.exercises.create!(category: 'conditioning', name:'1.5 mile run', value: 10.6967)
+        @power = @profile.exercises.create!(category: 'conditioning', name:'400m run', value: 1.896)
     end 
 
-    it 'should show strenght results' do
+    it 'should show strength results' do
         visit '/results'
 
         expect(page).to have_content("SOF Land Contract Assessment Results")
         expect(page).to have_content("Overall Score: 75")
+        expect(page).to have_content("Strength Score: 99")
         expect(page).to have_content("Lower Body Score: 100")
         expect(page).to have_content("Upper Body Score: 97")
         expect(page).to have_content(@squat.value.round(0))
@@ -36,5 +43,23 @@ RSpec.describe 'the landing page' do
         expect(page).to have_content(@press.name)
         expect(page).to have_content(@weighted_pullup.value.round(0))
         expect(page).to have_content(@weighted_pullup.name)
+    end 
+
+    it 'should show conditioning results' do
+        visit '/results'
+        
+        expect(page).to have_content("Conditioning Score: 74")
+        expect(page).to have_content("Extended Capacity Score: 78")
+        expect(page).to have_content("Capacity Score: 99")
+        expect(page).to have_content("Extended Power Score: 66")
+        expect(page).to have_content("Power Score: 55")
+        expect(page).to have_content("2 hours 0 minutes")
+        expect(page).to have_content(@extended_capacity.name)
+        expect(page).to have_content("35 minutes 30 seconds")
+        expect(page).to have_content(@capacity.name)
+        expect(page).to have_content("10 minutes 42 seconds")
+        expect(page).to have_content(@extended_power.name)
+        expect(page).to have_content("1 minutes 54 seconds")
+        expect(page).to have_content(@power.name)
     end 
 end 
