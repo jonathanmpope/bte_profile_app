81d3485207c05ecd80c31b4e982d8ec3ff09a4d2
jonathanmpope <jonathanmpope@gmail.com>
2022-12-05 19:15:30 -0700

    feat: add mar contract profile results show page feature test

--- spec/features/results/show_spec.rb
+++ spec/features/results/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the landing page' do
     before :each do 
         visit '/'
 
-        user = User.create!(email: 'test@test.com', password: "test", password_confirmation: "test")
+        @user = User.create!(email: 'test@test.com', password: "test", password_confirmation: "test")
 
         password = "test"
         @email = 'test@test.com'
@@ -13,8 +13,10 @@ RSpec.describe 'the landing page' do
         fill_in :password, with: password
 
         click_on "Login"
+    end 
 
-        @profile = user.profiles.create!(weight: 195.0, track: "sof_land_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
+    it 'should show strength results for sof land contract profile' do
+        @profile = @user.profiles.create!(weight: 195.0, track: "sof_land_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
         
         @squat = @profile.exercises.create!(category: 'strength', name: "Back Squat", value: 355)
         @deadlift = @profile.exercises.create!(category: 'strength', name: "Trap Bar", value: 455)
@@ -29,9 +31,7 @@ RSpec.describe 'the landing page' do
         @pushups = @profile.exercises.create!(category: 'work capacity', name:'Pushups', value: 55)
         @pullups = @profile.exercises.create!(category: 'work capacity', name:'Pull-ups', value: 12)
         @hang = @profile.exercises.create!(category: 'work capacity', name:'Hang', value: 1.5)
-    end 
 
-    it 'should show strength results' do
         visit '/results'
 
         expect(page).to have_content("SOF Land Contract Assessment Results")
@@ -49,7 +49,23 @@ RSpec.describe 'the landing page' do
         expect(page).to have_content(@weighted_pullup.name)
     end 
 
-    it 'should show conditioning results' do
+    it 'should show conditioning results for a sof land contract profile' do
+        @profile = @user.profiles.create!(weight: 195.0, track: "sof_land_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
+        
+        @squat = @profile.exercises.create!(category: 'strength', name: "Back Squat", value: 355)
+        @deadlift = @profile.exercises.create!(category: 'strength', name: "Trap Bar", value: 455)
+        @press = @profile.exercises.create!(category: 'strength', name: "Bench Press", value: 275)
+        @weighted_pullup = @profile.exercises.create!(category: 'strength', name: "Weighted Pull-up", value: 260)
+
+        @extended_capacity = @profile.exercises.create!(category: 'conditioning', name:'8 mile ruck', value: 120)
+        @capacity = @profile.exercises.create!(category: 'conditioning', name:'5 mile run', value: 35.5)
+        @extended_power = @profile.exercises.create!(category: 'conditioning', name:'1.5 mile run', value: 10.6967)
+        @power = @profile.exercises.create!(category: 'conditioning', name:'400m run', value: 1.896)
+
+        @pushups = @profile.exercises.create!(category: 'work capacity', name:'Pushups', value: 55)
+        @pullups = @profile.exercises.create!(category: 'work capacity', name:'Pull-ups', value: 12)
+        @hang = @profile.exercises.create!(category: 'work capacity', name:'Hang', value: 1.5)
+
         visit '/results'
         
         expect(page).to have_content("Conditioning Score: 74")
@@ -67,7 +83,23 @@ RSpec.describe 'the landing page' do
         expect(page).to have_content(@power.name)
     end 
 
-    it 'should show work capacity results' do
+    it 'should show work capacity results for a sof land contract profile' do
+        @profile = @user.profiles.create!(weight: 195.0, track: "sof_land_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
+        
+        @squat = @profile.exercises.create!(category: 'strength', name: "Back Squat", value: 355)
+        @deadlift = @profile.exercises.create!(category: 'strength', name: "Trap Bar", value: 455)
+        @press = @profile.exercises.create!(category: 'strength', name: "Bench Press", value: 275)
+        @weighted_pullup = @profile.exercises.create!(category: 'strength', name: "Weighted Pull-up", value: 260)
+
+        @extended_capacity = @profile.exercises.create!(category: 'conditioning', name:'8 mile ruck', value: 120)
+        @capacity = @profile.exercises.create!(category: 'conditioning', name:'5 mile run', value: 35.5)
+        @extended_power = @profile.exercises.create!(category: 'conditioning', name:'1.5 mile run', value: 10.6967)
+        @power = @profile.exercises.create!(category: 'conditioning', name:'400m run', value: 1.896)
+
+        @pushups = @profile.exercises.create!(category: 'work capacity', name:'Pushups', value: 55)
+        @pullups = @profile.exercises.create!(category: 'work capacity', name:'Pull-ups', value: 12)
+        @hang = @profile.exercises.create!(category: 'work capacity', name:'Hang', value: 1.5)
+
         visit '/results'
         
         expect(page).to have_content("Work Capacity Score: 53")
@@ -77,5 +109,36 @@ RSpec.describe 'the landing page' do
         expect(page).to have_content("12 reps")
         expect(page).to have_content(@hang.name)
         expect(page).to have_content("1 minute(s) 30 seconds")
+    end 
+
+     it 'should show results for sof maritime contract profile' do
+        @profile = @user.profiles.create!(weight: 195.0, track: "sof_mar_cont", strength_lower_score: 100.0, strength_upper_score: 97.06959706959707, strength_score: 98.53479853479854, conditioning_extended_capacity_score: 77.77777777777777, conditioning_capacity_score: 98.75776397515527, conditioning_extended_power_score: 66.15384615384615, conditioning_power_score: 54.666666666666664, conditioning_score: 74.33901364336147, work_capacity_score: 53.464999999999996)
+        
+        @squat = @profile.exercises.create!(category: 'strength', name: "Back Squat", value: 355)
+        @deadlift = @profile.exercises.create!(category: 'strength', name: "Trap Bar", value: 455)
+        @press = @profile.exercises.create!(category: 'strength', name: "Bench Press", value: 275)
+        @weighted_pullup = @profile.exercises.create!(category: 'strength', name: "Weighted Pull-up", value: 260)
+
+        @extended_capacity = @profile.exercises.create!(category: 'conditioning', name:'8 mile ruck', value: 120)
+        @capacity = @profile.exercises.create!(category: 'conditioning', name:'5 mile run', value: 35.5)
+        @capacity_swim = @profile.exercises.create!(category: 'conditioning', name:'2000m swim', value: 10.5)
+        @extended_power = @profile.exercises.create!(category: 'conditioning', name:'1.5 mile run', value: 10.6967)
+        @extended_power_swim = @profile.exercises.create!(category: 'conditioning', name:'500m swim', value: 10.6967)
+        @power = @profile.exercises.create!(category: 'conditioning', name:'400m run', value: 1.896)
+
+        @pushups = @profile.exercises.create!(category: 'work capacity', name:'Pushups', value: 55)
+        @pullups = @profile.exercises.create!(category: 'work capacity', name:'Pull-ups', value: 12)
+        @hang = @profile.exercises.create!(category: 'work capacity', name:'Hang', value: 1.5)
+
+        visit '/results'
+
+        expect(page).to have_content("SOF Maritime Contract Assessment Results")
+
+        expect(page).to have_content("Overall Score: 75")
+        expect(page).to have_content("Strength Score: 99")
+        expect(page).to have_content("Conditioning Score: 74")
+        expect(page).to have_content("Work Capacity Score: 53")
+        expect(page).to have_content("2000m swim: 10 minutes 30 seconds")
+        expect(page).to have_content("500m swim: 10 minutes 42 seconds")
     end 
 end 
