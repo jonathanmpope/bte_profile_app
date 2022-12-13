6622d42aa5f61fd8f48d6ac7f96b78efd28c42b8
jonathanmpope <jonathanmpope@gmail.com>
2022-12-12 18:35:40 -0700

    ref: update profile logic so scores cant be below zero

--- spec/features/profile/new_spec.rb
+++ spec/features/profile/new_spec.rb
@@ -96,6 +96,43 @@ RSpec.describe 'the landing page' do
         expect(page).to have_content("SOF Maritime Contract Assessment Results")
     end 
 
+    it 'should be a page with several options and lets you select sof maritime contract track with front squat, straigh bar dl, and oh press' do
+        select("SOF Maritime Contract", from: "track")  
+        click_button("Select Track")
+        
+        expect(page).to have_content("SOF Maritime Contract Assessments")
+
+        fill_in("ruck_hours", with:2)
+        fill_in("ruck_minutes", with:15)
+        fill_in("five_mile_minutes", with:40)
+        fill_in("five_mile_seconds", with:15)
+        fill_in("two_thousand_swim_minutes", with:40)
+        fill_in("two_thousand_swim_seconds", with:15)
+        fill_in("one_and_half_mile_run_minutes", with:10)
+        fill_in("one_and_half_mile_run_seconds", with:50)
+        fill_in("five_hundred_swim_minutes", with:11)
+        fill_in("five_hundred_swim_seconds", with:50)
+        fill_in("four_hundred_run_minutes", with:1)
+        fill_in("four_hundred_run_seconds", with:30)
+        fill_in("pushup_reps", with:50)
+        fill_in("pullup_reps", with:12)
+        fill_in("hang_minutes", with:1)
+        fill_in("hang_seconds", with:19)
+        choose(id="units_imperial")
+        fill_in("body_weight", with:195)
+        select("Front Squat", from: "squat")
+        fill_in("squat_weight", with:276) 
+        select("Straight Bar", from: "deadlift")
+        fill_in("deadlift_weight", with:338)  
+        select("Overhead Press (1 Arm)", from: "press")
+        fill_in("press_weight", with:76)
+        fill_in("pullup_weight", with:265)
+        click_button("Submit Results")
+
+        expect(current_path).to eq("/results")
+        expect(page).to have_content("SOF Maritime Contract Assessment Results")
+    end 
+
     it 'should be a page with several options and lets you select sof land prep track' do
         select("SOF Land Prep", from: "track")  
         click_button("Select Track")
@@ -191,5 +228,43 @@ RSpec.describe 'the landing page' do
 
         expect(current_path).to eq("/results")
         expect(page).to have_content("SOF Maritime Prep Assessment Results")
+    end
+
+    it 'will not have a score less than zero' do
+        select("SOF Maritime Prep", from: "track")  
+        click_button("Select Track")
+        
+        expect(page).to have_content("SOF Maritime Prep Assessments")
+        select("3 Mile Run", from: "prep_run_or_ruck")
+        fill_in("ruck_or_run_hours", with:1)
+        fill_in("ruck_or_run_minutes", with:20)
+        fill_in("one_and_half_mile_run_minutes", with:20)
+        fill_in("one_and_half_mile_run_seconds", with:50)
+        fill_in("five_hundred_swim_minutes", with:20)
+        fill_in("five_hundred_swim_seconds", with:50)
+        fill_in("four_hundred_run_minutes", with:2)
+        fill_in("four_hundred_run_seconds", with:30)
+        fill_in("pushup_reps", with:20)
+        fill_in("pullup_reps", with:3)
+        fill_in("hang_minutes", with:0)
+        fill_in("hang_seconds", with:19)
+        choose(id="units_imperial")
+        fill_in("body_weight", with:200)
+        select("Back Squat", from: "squat")
+        fill_in("squat_weight", with:106) 
+        select("Trap Bar", from: "deadlift")
+        fill_in("deadlift_weight", with:163)  
+        select("Bench Press", from: "press")
+        fill_in("press_weight", with:114)
+        fill_in("pullup_weight", with:167)
+        click_button("Submit Results")
+
+        expect(current_path).to eq("/results")
+        
+        expect(page).to have_content("SOF Maritime Prep Assessment Results")
+        expect(page).to have_content("Overall Score: 5")
+        expect(page).to have_content("Strength Score: 13")
+        expect(page).to have_content("Conditioning Score: 2")
+        expect(page).to have_content("Work Capacity Score: 0")
     end
 end 
